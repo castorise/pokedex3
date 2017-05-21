a6f607c561ad6cf431376c73c0dbfea430152ceb
Chao Zheng <Castor@Chaos-MBP.fios-router.home>
2017-05-21 09:01:47 -0400

    Added asset

---
+++ pokedex3/Pokemon.swift
@@ -0,0 +1,32 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Chao Zheng on 5/18/17.
+//  Copyright © 2017 Castorise. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+
+
+}
